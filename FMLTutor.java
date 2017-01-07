256a68e5dedbcb14a17e2839a90282b70d9d4a7e
FourYi <1531276974@qq.com>
2017-01-07 17:51:58 +0800

    Create FMLTutor.java

---
+++ FMLTutor.java
@@ -0,0 +1,48 @@
+src/main/java/com/github/FourYi/fmltutor/FMLTutor.java:
+
+package com.github.FourYi.fmltutor;
+
+import com.github.FourYi.fmltutor.common.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * @author FourYi
+ */
+@Mod(modid = FMLTutor.MODID, name = FMLTutor.NAME, version = FMLTutor.VERSION, acceptedMinecraftVersions = "1.8.9")
+public class FMLTutor
+{
+    public static final String MODID = "fmltutor";
+    public static final String NAME = "FML Tutor";
+    public static final String VERSION = "1.0.0";
+
+    @Instance(FMLTutor.MODID)
+    public static FMLTutor instance;
+
+    @SidedProxy(clientSide = "com.github.FourYi.fmltutor.client.ClientProxy", serverSide = "com.github.ustc_zzzz.fmltutor.common.CommonProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
+    }
+}
